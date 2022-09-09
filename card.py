da756fcba7d85ab816646b7236fbb3be86444456
Joe-SL <jphabmixay@DESKTOP-QUTMJP0>
2022-09-08 21:39:55 -0700

    First Commit

---
+++ card.py
@@ -0,0 +1,18 @@
+import ctypes
+
+
+class Card:
+    cid = None
+    name = None
+    cost = None
+    ctype = None
+    desc = None
+
+
+    def __init__(self, cid, name, cost, cytpe, desc) -> None:
+        this.cid = cid
+        this.name = name
+        this.cost = cost
+        this.ctype = ctypes
+        this.desc = desc
+        pass
